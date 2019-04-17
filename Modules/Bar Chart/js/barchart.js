b33a72d3f86e995872241acfe838a7d852aedd76
Colin Thompson <colinmthomps@gmail.com>
2019-04-16 20:04:19 -0400

    barchart

--- Modules/Bar Chart/js/barchart.js
+++ Modules/Bar Chart/js/barchart.js
@@ -1,57 +1,136 @@
-var width = document.querySelector("#barchart").clientWidth;
-var height = 400;
-var margin = {top:50,left:100,right:25,bottom:25};
+var width = 325;
+var height = 550;
+var margin = {top: 75, left: 25, right: 60, bottom: 150}
+
 
 var svg = d3.select("#barchart")
   .append("svg")
-  .attr("width","100%")
-  .attr("height","100%")
-  .attr("viewBox", "0 0 700 500")
-  .attr("preserveAspectRatio","xMinYMin");
+  .attr("width",width)
+  .attr("height",height)
+  .append("g");
 
-var data = [
-  {sexOr: "Straight", appUse: 28},
-  {sexOr: "Gay", appUse: 49}
-];
+var grp = svg.append("g")
+  .attr("transform", "translate(0,0)");
 
-var yScale = d3.scaleBand()
-  .domain(["Straight","Gay"])
-  .rangeRound([margin.top,height-margin.bottom])
-  .padding(0.25);
+var tooltip = d3.select("body")
+  .append("div")
+  .style("position", "absolute")
+  .style("z-index", "10")
+  .style("visibility", "hidden")
+  .style("background-color", "#FFB350")
+  .style("font-family","Helvetica")
+  .style("font-size","9pt");
 
-var xScale = d3.scaleLinear()
-  .domain([0,100])
-  .range([margin.left,width-margin.right]);
+var phone = grp.append("rect")
+  .attr("rx",20)
+  .attr("ry",20)
+  .attr("x",0)
+  .attr("y",0)
+  .attr("class",".barRect")
+  .attr("width", 325)
+  .attr("height", 550)
+  .attr("transform", function(d, i) { return "scale(" + (1 - d / 25) * 20 + ")"; })
+  .attr("fill","#000000");
 
-var xAxis = svg.append("g")
-  .attr("transform",`translate(0,${height-margin.bottom})`)
-  .call(d3.axisBottom().scale(xScale));
+var displayBase = grp.append("rect")
+  .attr("x",10)
+  .attr("y",50)
+  .attr("width",305)
+  .attr("height",425)
+  .attr("transform", function(d, i) { return "scale(" + (1 - d / 25) * 20 + ")"; })
+  .attr("fill","#FF6A5A");
 
-var yAxis = svg.append("g")
-  .attr("transform",`translate(${margin.left},0)`)
-  .call(d3.axisLeft().scale(yScale));
+var phoneButton = grp.append("circle")
+  .attr("cx",162.5)
+  .attr("cy",510)
+  .attr("r",20)
+  .attr("stroke-width",3)
+  .attr("stroke","#353031")
+  .attr("fill","#000000");
 
-var label = svg.append("text")
-        .attr("x", (width / 2))
-        .attr("y", 0 + (margin.top/2))
-        .attr("text-anchor", "middle")
-        .style("font-size", "16px")
-        .text("Dating App Usage by Sexual Orientation");
+var camera = grp.append("circle")
+  .attr("cx",100)
+  .attr("cy",25)
+  .attr("r",5)
+  .attr("fill","#353031");
 
-var bar = svg.selectAll("rect")
-  .data(data)
-  .enter()
-  .append("rect")
-    .attr("class","appUseBar")
-    .attr("x",margin.left)
-    .attr("y", function(d) {return yScale(d.sexOr);})
-    .attr("width",function(d) {return xScale(d.appUse) - margin.left; })
-    .attr("height", yScale.bandwidth())
-    .attr("fill","#83B8AA")
-    .on("mouseover", function() {
-      d3.select(this)
-        .attr("fill","#FBB252");
-    }).on("mouseout", function() {
-      d3.select(this)
-        .attr("fill","#83B8AA");
-    })
+var speaker = grp.append("rect")
+  .attr("rx",2.5)
+  .attr("ry",2.5)
+  .attr("x",125)
+  .attr("y",21)
+  .attr("width", 75)
+  .attr("height", 7.5)
+  .attr("transform", function(d, i) { return "scale(" + (1 - d / 25) * 20 + ")"; })
+  .attr("fill","#353031");
+
+  var data = [
+    {x:"Straight",y:28},
+    {x:"Gay",y:49}
+  ];
+
+  var xScale = d3.scaleBand()
+    .domain(["Straight","Gay"])
+    .rangeRound([margin.left*2, width-margin.right*2])
+    .padding(0.5);
+  var yScale = d3.scaleLinear()
+    .domain([0,100])
+    .range([height-margin.bottom,margin.top*2]);
+
+  var xAxis = svg.append("g")
+    .attr("transform", `translate (${margin.left},${height-margin.bottom})`)
+    .call(d3.axisBottom().scale(xScale));
+
+  var yAxis = svg.append("g")
+    .attr("transform", `translate(${margin.left*3},0)`)
+    .call(d3.axisLeft().scale(yScale));
+
+  var xLabel = svg.append("text")
+    .attr("class","axisLabel")
+    .attr("x", width/2)
+    .attr("y", margin.top*1.25)
+    .attr("text-anchor","middle")
+    .attr("font-size","9pt")
+    .attr("font-family","Helvetica")
+    .text("Dating Application Usage by Sexual Orientation")
+
+  var yLabel = svg.append("text")
+    .attr("class","axisLabel")
+    .attr("transform","rotate(-90)")
+    .attr("y",margin.left*1.5)
+    .attr("x",-height/2)
+    .attr("text-anchor","middle")
+    .attr("font-size","8pt")
+    .attr("font-family","Helvetica")
+    .text("Percentage that Uses Dating Apps")
+
+  var bar = svg.selectAll(".barRect")
+    .data(data)
+    .enter()
+    .append("rect")
+      .attr("class", "bar")
+      .attr("x",function(d) {return xScale(d.x)+margin.left;})
+      .attr("y", function(d) {return yScale(d.y); })
+      .attr("width", xScale.bandwidth())
+      .attr("height",function(d){return height - margin.bottom - yScale(d.y); })
+      .attr("fill", "#272D4D")
+      .on("mouseover", function(d) {
+        d3.select(this)
+          .attr("fill", "#FFB350");
+        tooltip.style("visibility", "visible")
+        tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px")
+        tooltip.html((d.y) + " Percent")
+      }) .on("mouseout", function(d) {
+        d3.select(this)
+          .attr("fill", "#272D4D")
+        tooltip.style("visibility", "hidden")
+      });
+
+  var source = svg.append("text")
+    .attr("class","source")
+    .attr("x",width/2)
+    .attr("y",margin.bottom*3)
+    .attr("text-anchor","middle")
+    .attr("font-size","6pt")
+    .attr("font-family","Helvetica")
+    .text("Source: 2017 survey conducted by Clue & the Kinsey Institute")
